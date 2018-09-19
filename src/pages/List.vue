9b6713f01ad42ef4500ffbbd037e7b5429d82ed3
tiandaoyi <495060071@qq.com>
2018-09-20 00:35:06 +0800

    router

---
+++ src/pages/List.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="list">
+      {{msg}}
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'List',
+  data () {
+    return {msg: 'this is list info'}
+  }
+}
+</script>
