bf2d225bae06b773ca5ffdf256f0a4d1f8c634b1
tiandaoyi <495060071@qq.com>
2018-09-21 00:38:08 +0800

    add header

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
+    <home-header></home-header>
     <div class="home">
       {{msg}}
     </div>
   </div>
 </template>
 <script>
+import HomeHeader from './components/Header'
 export default {
   name: 'home',
+  components: {
+    HomeHeader
+  },
   data () {
     return {msg: 'this is home'}
   }
