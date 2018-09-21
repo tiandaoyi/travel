3b2d53e4fe143dc8b762d9af1c90b9556696a714
tiandaoyi <495060071@qq.com>
2018-09-21 09:50:34 +0800

    xx

---
+++ src/pages/home/components/header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="home">
+      {{msg}}
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeHeader',
+  data () {
+    return {msg: 'this is HomeHeader'}
+  }
+}
+</script>
