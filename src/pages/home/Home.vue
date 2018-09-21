d6e4c806f2b1ec3bac9103bffd4c1f5032080475
tiandaoyi <495060071@qq.com>
2018-09-21 17:59:38 +0800

    index-icons

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,6 +2,7 @@
   <div>
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <home-icons></home-icons>
     <div class="home">
       {{msg}}
     </div>
@@ -10,11 +11,14 @@
 <script>
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
+
 export default {
   name: 'home',
   components: {
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeIcons
   },
   data () {
     return {msg: 'this is home'}
--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -5,7 +5,6 @@
       <swiper-slide v-for="item of swiperList" :key="item.id">
         <img class="swiper-img" :src="item.imgUrl" alt="">
       </swiper-slide>
-      <!-- Optional controls -->
       <div class="swiper-pagination" slot="pagination"></div>
       <!--<div class="swiper-button-prev" slot="button-prev"></div>-->
       <!--<div class="swiper-button-next" slot="button-next"></div>-->
