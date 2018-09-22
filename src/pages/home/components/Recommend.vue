2a105c7b0b76d283e7a6c500045dfc2584ebe357
tiandaoyi <495060071@qq.com>
2018-09-22 21:37:02 +0800

    recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <div class="recommend-title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <img :src="item.imgUrl" alt="" class="item-img">
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1609/80/8092254b7f5f016ea3.water.jpg_200x200_cd798d39.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫的海洋主题乐园'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1609/80/8092254b7f5f016ea3.water.jpg_200x200_cd798d39.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫的海洋主题乐园'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1609/80/8092254b7f5f016ea3.water.jpg_200x200_cd798d39.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫的海洋主题乐园'
+      }]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .recommend-title
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item
+    overflow: hidden
+    height: 1.9rem
+    display: flex
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      padding: .1rem
+      flex: 1
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        font-size: .28rem
+        color: #ccc
+        ellipsis()
+      .item-button
+        line-height: .44rem
+        margin-top: .16rem
+        background: #ff9300
+        padding: 0 .1rem
+        border-radius: .06rem
+        color: #fff
+        ellipsis()
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <div class="recommend-title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img :src="item.imgUrl" alt="" class="item-img">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/7d/df6ff468331c43.jpg_r_640x214_1f3783d0.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫的海洋主题乐园'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/7d/df6ff468331c43.jpg_r_640x214_1f3783d0.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫的海洋主题乐园'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/7d/df6ff468331c43.jpg_r_640x214_1f3783d0.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫的海洋主题乐园'
+      }]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .recommend-title
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item
+    overflow: hidden
+    /*height: 1.9rem*/
+    /*display: flex*/
+    .item-img-wrapper
+      overflow: hidden
+      height: 0
+      width: 100%
+      padding-bottom: 33.9%
+      .item-img
+        width: 100%
+    .item-info
+      padding: .1rem
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        font-size: .28rem
+        color: #ccc
+        ellipsis()
+</style>
