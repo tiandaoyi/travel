e89832fcfa9b4bf41e047501734ab300a798056f
tiandaoyi <495060071@qq.com>
2018-09-23 20:49:21 +0800

    city router

---
+++ src/pages/city/components/cityHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+      <div class="iconfont back-icon header-back">&#xe624;</div>
+    </router-link>
+  </div>
+</template>
+<script>
+export default {
+  name: 'cityHeader'
+}
+</script>
+<style lang="stylus" scoped="">
+  @import '~styles/varibles'
+  .header
+    position: relative
+    overflow: hidden
+    height: $headerHeight
+    line-height: $headerHeight
+    text-align: center
+    color: #fff
+    background: $bgColor
+    font-size: .32rem
+    .header-back
+      position: absolute
+      left: 0
+      top: 0
+      width: .64rem
+      text-align: center
+      fint-size: .4rem
+      color: #fff
+</style>
